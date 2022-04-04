272d79fbf8a9f99a948a31a94c3e666ddeaa9b35
MarkClancySutton <101901359+MarkClancySutton@users.noreply.github.com>
2022-04-05 00:03:38 +0100

    betterattempt
    tried harder

---
+++ assignment/Control.java
@@ -0,0 +1,18 @@
+package assignment;
+
+
+public class Control {
+
+	public static void main(String[] args) {
+		
+		TestHashSet file1 = new TestHashSet();
+		TestHashSet file2 = new TestHashSet();
+		file1.readwords("example.txt",10);
+		file2.readwords("writing.txt",10);
+	
+		
+		
+		
+	}
+
+}
